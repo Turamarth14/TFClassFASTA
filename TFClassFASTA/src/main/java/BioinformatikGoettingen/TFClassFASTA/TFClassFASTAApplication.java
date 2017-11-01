b20d71aa4cbf3401bdb13b41656434d60c062252
Turamarth14 <felixheinrich@freenet.de>
2017-11-01 13:13:18 +0100

    Initializing project with Dropwizard archetype

---
+++ TFClassFASTA/src/main/java/BioinformatikGoettingen/TFClassFASTA/TFClassFASTAApplication.java
@@ -0,0 +1,29 @@
+package BioinformatikGoettingen.TFClassFASTA;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class TFClassFASTAApplication extends Application<TFClassFASTAConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new TFClassFASTAApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "TFClassFASTA";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<TFClassFASTAConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final TFClassFASTAConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
---
+++ TFClassFASTA/src/main/java/BioinformatikGoettingen/TFClassFASTA/TFClassFASTAConfiguration.java
@@ -0,0 +1,10 @@
+package BioinformatikGoettingen.TFClassFASTA;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class TFClassFASTAConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
