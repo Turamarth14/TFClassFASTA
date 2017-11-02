5537ec4506dded5ce399a25f8f03e8b34a6afdfc
Turamarth14 <felixheinrich@freenet.de>
2017-11-02 13:22:45 +0100

    Added part of Hibernate configuration

--- TFClassFASTA/src/main/java/BioinformatikGoettingen/TFClassFASTA/TFClassFASTAApplication.java
+++ TFClassFASTA/src/main/java/BioinformatikGoettingen/TFClassFASTA/TFClassFASTAApplication.java
@@ -1,11 +1,25 @@
 package BioinformatikGoettingen.TFClassFASTA;
 
+import com.google.common.base.Charsets;
+
+import BioinformatikGoettingen.TFClassFASTA.api.Fasta;
+import de.thomaskrille.dropwizard_template_config.TemplateConfigBundle;
+import de.thomaskrille.dropwizard_template_config.TemplateConfigBundleConfiguration;
 import io.dropwizard.Application;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class TFClassFASTAApplication extends Application<TFClassFASTAConfiguration> {
-
+	
+	private final HibernateBundle<TFClassFASTAConfiguration> hibernate = new HibernateBundle<TFClassFASTAConfiguration>(Fasta.class) {
+		@Override
+		public DataSourceFactory getDataSourceFactory(TFClassFASTAConfiguration configuration) {
+			return configuration.getDataSourceFactory();
+		}
+	};
+	
     public static void main(final String[] args) throws Exception {
         new TFClassFASTAApplication().run(args);
     }
@@ -17,7 +31,8 @@ public class TFClassFASTAApplication extends Application<TFClassFASTAConfigurati
 
     @Override
     public void initialize(final Bootstrap<TFClassFASTAConfiguration> bootstrap) {
-        // TODO: application initialization
+        bootstrap.addBundle(hibernate);
+        bootstrap.addBundle(new TemplateConfigBundle(new TemplateConfigBundleConfiguration().charset(Charsets.US_ASCII)));
     }
 
     @Override
--- TFClassFASTA/src/main/java/BioinformatikGoettingen/TFClassFASTA/TFClassFASTAConfiguration.java
+++ TFClassFASTA/src/main/java/BioinformatikGoettingen/TFClassFASTA/TFClassFASTAConfiguration.java
@@ -1,10 +1,23 @@
 package BioinformatikGoettingen.TFClassFASTA;
 
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.Valid;
+
 import org.hibernate.validator.constraints.*;
-import javax.validation.constraints.*;
+
 
 public class TFClassFASTAConfiguration extends Configuration {
-    // TODO: implement service configuration
+    @Valid
+    @NotEmpty
+    @JsonProperty("database")
+    private DataSourceFactory database = new DataSourceFactory();
+    
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory(){
+        return database;
+    }
 }
